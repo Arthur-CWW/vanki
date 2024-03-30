694ed2356d81d68b37c9167ebbf73eef58634cad
chun1 <arthur@chunai.dev>
2024-03-30 18:28:01 +1100

    python env

--- api/index.py
+++ api/index.py
@@ -1,26 +1,43 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI()
-from pydantic_settings import BaseSettings
-from pydantic import AnyHttpUrl
+# app = FastAPI()
+# from pydantic_settings import BaseSettings
+# from pydantic import AnyHttpUrl
 
 # class Settings(BaseSettings):
 #     database_url: AnyHttpUrl
 #     nextauth_url: AnyHttpUrl
 #     model_config = S
 
+from pydantic import AnyHttpUrl, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from pydantic.env_settings import build_from_env
 
 
 class Settings(BaseSettings):
     # nextauth_url: AnyHttpUrl
     model_config = SettingsConfigDict(env_file='../.env', env_file_encoding='utf-8')
-    database_url: AnyHttpUrl
+    DATABASE_URL: Field(AnyHttpUrl, env='DATABASE_URL')
+
+from litestar import Litestar, get
+#
+
+# @app.get("/api/python")
+# def hello_world():
+#     return {"message": "Hello World"}
+
+@get("/")
+async def index() -> str:
+    # return "Hello, world!"
+    settings = Settings()
+    print(settings.model_config)
+    print(settings)
+    return settings.model_dump_json()
 
 
+@get("/books/{book_id:int}")
+async def get_book(book_id: int) -> dict[str, int]:
+    return {"book_id": book_id}
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+
+app = Litestar([index, get_book])
