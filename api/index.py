8d61765d2c6ee5c14a2161c8f59486e86f9ca107
chun1 <arthur@chunai.dev>
2024-04-06 17:37:27 +1100

    change to fastapi

--- api/index.py
+++ api/index.py
@@ -1,14 +1,4 @@
-# from fastapi import FastAPI
-
-# app = FastAPI()
-# from pydantic_settings import BaseSettings
-# from pydantic import AnyHttpUrl
-
-# class Settings(BaseSettings):
-#     database_url: AnyHttpUrl
-#     nextauth_url: AnyHttpUrl
-#     model_config = S
-
+from fastapi import FastAPI
 from pydantic import AnyHttpUrl, AnyUrl, BaseModel, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -18,23 +8,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: AnyUrl
-    nextauth_url: AnyHttpUrl
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    database_url: AnyUrl= Field(default=...)
+    nextauth_url: AnyHttpUrl= Field(default=...)
+    # model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
 
 
-
-from litestar import Litestar, get, post, put, delete
-
-
-@get("/")
-async def index() -> str:
-    settings = Settings() # type: ignore
-
-    print(settings.database_url, settings.nextauth_url, settings.model_config)
-    return settings.model_dump_json( )
-
-# from dataclasses import dataclass
 
 class Card(BaseModel):
     id: int
@@ -42,13 +23,19 @@ class Card(BaseModel):
     description: str
 
 
+app = FastAPI()
 
-@post("/cards")
-def create_card(card: Card) -> Card:
+@app.get("/")
+async def index() -> dict:
+    settings = Settings()
+    print(settings.database_url, settings.nextauth_url, settings.model_config)
+    return settings.model_dump( )
+
+@app.post("/cards", response_model=Card)
+async def create_card(card: Card) -> Card:
     print(card)
     return card
 
-@get("/cards/{card}")
-def get_cards(numb: int) -> Card:
-    return Card(id=numb, name="Card", description="Card description")
-app = Litestar([index, create_card])
+@app.get("/cards/{card_id}", response_model=Card)
+async def get_cards(card_id: int) -> Card:
+    return Card(id=card_id, name="Card", description="Card description")
